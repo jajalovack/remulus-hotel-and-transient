c5c619df22a87d382d901466bd711aca53819f7e
ariesjay24 <ariescuevas063@gmail.com>
2023-09-28 00:44:20 +0800

    register and login

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,20 +1,30 @@
-import Home from "./views/Home/index"
-import About from "./views/About/index"
-import HotelList from "./views/HotelList/index"
+import Home from "./views/Home/index";
+import About from "./views/About/index";
+import HotelList from "./views/HotelList/index";
+import Login from "./views/LogIn/Login";
+import Register from "./views/Register/Register";
 
-const routes=[
-    {
-        path: "/",
-        element: <Home/>
-    },
-    {
-        path: "/about",
-        element: <About/>
-    },
-    {
-        path: "/book",
-        element: <HotelList/>
-    }
+const routes = [
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+  {
+    path: "/book",
+    element: <HotelList />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
 ];
 
 export default routes;
