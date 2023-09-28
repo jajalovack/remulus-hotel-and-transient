12501bb2b5e587b2a51f4018930b3736c2c07548
jajalovack <dvtejuco@gmail.com>
2023-09-28 21:46:25 +0800

    hero section complete

--- src/sections/Home/Hero.jsx
+++ src/sections/Home/Hero.jsx
@@ -1,10 +1,13 @@
 import "./Hero.scoped.css";
+import image from "../../assets/home/heroImage.jpg";
+import logo from "../../assets/logo.svg"
 
 const HomeHero = () => {
   return (
     <>
         <section className="hero">
-
+          <img src={image}/>
+          <object data={logo}></object>
         </section>
     </>
   )
